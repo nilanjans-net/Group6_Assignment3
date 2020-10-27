9026006ddb41f1e8a08aae6cffd6b3f95b56803f
Alex Zhideev <ZAlex1988@gmail.com>
2020-10-26 23:17:38 -0400

    NPS API Service

---
+++ Scripts/nps-api.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+	let endpoint = 'https://developer.nps.gov/api/v1/';
+	let apiKey = 'Xui3j5YQKjKLv7a5gqDYxeWdkMft7k9xdtduFSgt';
+	
+	$.ajaxSetup({
+		headers: { 'X-Api-Key': apiKey }
+	});
+
+	$.ajax({
+        url: endpoint + "parks?parkCode=arch,glac,ever",		
+        contentType: "application/json",
+        dataType: 'json',
+        success: function(result){
+			console.log("HERE");
+			//$('#parksTable thead').append(`<tt><td>Park Name</td><td>State</td><td>Description</td><td>URL</td></tt>`);
+			console.log("HERE2");
+			result.data.forEach(park => {
+				$('#parksTable').append(`<tr><td>${park.fullName}</td><td>${park.states}</td><td>${park.description}</td><td>${park.url}</td></tr>`);
+			});
+            
+        }
+    })  
+  
+});
